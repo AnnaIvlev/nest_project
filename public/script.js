ce0b8fb557c0539c837eeb35d323828924259610
Anna Veber <53593640+AnnaIvlev@users.noreply.github.com>
2023-11-01 10:07:49 -0400

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,7 @@
+document.getElementById('addProductButton').addEventListener('click', async () => {
+  const userName = document.getElementById('userName').value;
+  const productName = document.getElementById('productName').value;
+
+  // Use JavaScript fetch or an HTTP library to send a request to your GraphQL API.
+  // Handle the logic for adding a product to a user here.
+});
